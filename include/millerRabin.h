71ca1b47414cfe5a9f947496f37dc5d23f90d2f2
Aleksa Simić <139248765+aleksasimic3@users.noreply.github.com>
2024-11-06 02:43:05 +0100

    Update millerRabin.h

--- include/millerRabin.h
+++ include/millerRabin.h
@@ -6,17 +6,11 @@
 #include <chrono>
 #include <numeric>
 
-#include <iostream> //temp
-
-//maybe rename this from millerRabin.h to primalityTest.h
+#include <iostream>
 
 //returns if n is prime or not, uses Miller Rabin primality test (deterministic)
 //https://en.wikipedia.org/wiki/Miller%E2%80%93Rabin_primality_test
-//large primes http://www.isthe.com/chongo/tech/math/prime/mersenne.html
-//miller rabin test sometimes finds one divisor
 bool isPrime(uint64_t n, uint64_t* divisor=nullptr); //accepts n <= 2^64 - 1
 bool isPrime(int n);
-
-//bool isPrime(unsigned _int128 n); //check if _int128 is defined
 
 #endif
